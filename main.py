73491b689c853b77b50d4e59a96f7b59a38e6fbb
DISCRUTER <mohitanand10204@gmail.com>
2026-04-15 15:03:12 +0000

    add window

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
 def main():
-    print("Hello from asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        # Calling log state
+        log_state()
+
+        # Checking for pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        # Filling the screen with default color
+        screen.fill('black')
+        
+        # Refresh screen
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
